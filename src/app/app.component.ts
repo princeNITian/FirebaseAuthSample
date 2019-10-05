73619a16761c725056a8f48e5b1f698793c60d56
Prince Raj <princeraj427@gmai.com>
2019-10-05 13:52:09 +0530

    Login with facebook, twitter, and github added via both was

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,46 @@ export class AppComponent implements OnInit {
     this.afAuth.authState
     .subscribe((user)=>console.log(user));
   }
-
+// google signin via popup returns promises
   googleSignInViaPopup() {
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
     .then((userCredentials) => console.log(userCredentials));
   }
-
+// google signin via redirect returns void promises we need to use ngOnInit()
   googleSignInViaRedirect(){
     this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider())
     .then((userCredentials) => console.log(userCredentials));
   }
+// facebook signin via popup
+  facebookSignInViaPopup() {
+    this.afAuth.auth.signInWithPopup(new auth.FacebookAuthProvider())
+      .then((userCredentials) => console.log(userCredentials));
+  }
+// facebook signin via redirect
+  facebookSignInViaRedirect() {
+    this.afAuth.auth.signInWithRedirect(new auth.FacebookAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
+
+// twitter signin via popup
+ twitterSignInViaPopup() {
+   this.afAuth.auth.signInWithPopup(new auth.TwitterAuthProvider())
+   .then((userCredentials) => console.log(userCredentials));
+ }
+// twitter signin via redirect
+  twitterSignInViaRedirect() {
+    this.afAuth.auth.signInWithRedirect(new auth.TwitterAuthProvider())
+      .then((userCredentials) => console.log(userCredentials));
+  }
+
+// github signin via popup
+githubSignInViaPopup() {
+  this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider())
+  .then((userCredentials) => console.log(userCredentials));
+}
+// github signin via redirect
+githubSignInViaRedirect() {
+  this.afAuth.auth.signInWithRedirect(new auth.GithubAuthProvider())
+  .then((userCredentials) => console.log(userCredentials));
+}
 }
