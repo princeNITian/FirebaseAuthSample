da84c12dfed4a40438ab6567766847d7a4621f50
Prince Raj <princeraj427@gmai.com>
2019-10-05 13:32:28 +0530

    SignIn with Google via popup and redirect added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FirebaseAuthSample';
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit(){
+    this.afAuth.authState
+    .subscribe((user)=>console.log(user));
+  }
+
+  googleSignInViaPopup() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
+
+  googleSignInViaRedirect(){
+    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider())
+    .then((userCredentials) => console.log(userCredentials));
+  }
 }
